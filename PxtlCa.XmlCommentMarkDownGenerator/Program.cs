de46c31ae9f7a9e7d21dc416a90b3adbf39cd349
mikelau13 <mikelau13@hotmail.com>
2018-05-29 10:30:27 -0400

    Fixed issues: missing <br> and <seealso>; when target directory does not already exist, create it first.

--- PxtlCa.XmlCommentMarkDownGenerator/Program.cs
+++ PxtlCa.XmlCommentMarkDownGenerator/Program.cs
@@ -18,6 +18,11 @@ namespace PxtlCa.XmlCommentMarkDownGenerator
                     Console.WriteLine(options.GetUsage());
                     return;
                 }
+                // for in case the target directory does not already exist, create it now
+                if (!System.IO.Directory.Exists(Path.GetDirectoryName(options.OutputFile)))
+                {
+                    System.IO.Directory.CreateDirectory(Path.GetDirectoryName(options.OutputFile));
+                }
                 // consume Options instance properties
                 var inReader = options.ConsoleIn
                     ? Console.In
